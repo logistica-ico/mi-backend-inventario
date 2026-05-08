0ca425874e6da8f1606725e7faaaa3a60428323d
logistica-ico <logistica@icocert.pe>
2026-05-08 09:46:18 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     'https://fascinating-taiyaki-debea6.netlify.app',
-    'http://localhost:3000',
+    'http://localhost:3000', 'http://localhost:3001',
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
